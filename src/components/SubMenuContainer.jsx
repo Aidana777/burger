c0b62ba1797eb3cc9734348aa25e7164cbb3c75b
Aidana777 <94773705+Aidana777@users.noreply.github.com>
2022-05-31 11:53:29 +0600

    Initial commit

---
+++ src/components/SubMenuContainer.jsx
@@ -0,0 +1,17 @@
+import { ChevronRightRounded } from "@mui/icons-material";
+
+function SubMenuContainer() {
+  return (
+    <div className="subMenuContianer">
+      <h3>Menu Category</h3>
+      <div className="viewAll">
+        <p>View All</p>
+        <i>
+          <ChevronRightRounded />
+        </i>
+      </div>
+    </div>
+  );
+}
+
+export default SubMenuContainer;
